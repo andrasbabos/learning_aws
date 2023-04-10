c7e39693ed5e986b740a8332f3404e02f0f9102f
Babos Andras <andrasbabos@gmail.com>
2023-04-10 11:07:46 +0200

    terraform fmt formatting

--- dvdstore/terraform/ec2.tf
+++ dvdstore/terraform/ec2.tf
@@ -1,7 +1,8 @@
 resource "aws_instance" "dvdstore" {
-  ami           = "ami-03d20f9dd906ec688"
-  instance_type = "t3.micro"
-  key_name = "dvdstore_deployment"
+  # 
+  ami                    = "ami-03d20f9dd906ec688"
+  instance_type          = "t3.micro"
+  key_name               = "dvdstore_deployment"
   vpc_security_group_ids = [aws_security_group.dvdstore.id]
 
   tags = {
