8d3c12467822961b05bfd9b58a532351e2f5cf55
Babos Andras <andrasbabos@gmail.com>
2023-04-12 11:44:59 +0200

    modified ec2 terraform to amzn2 and bigger instance

--- dvdstore/terraform/ec2.tf
+++ dvdstore/terraform/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "dvdstore" {
-  ami                    = "ami-0f960c8194f5d8df5"
-  instance_type          = "t3.micro"
+  ami                    = "ami-0cbfcdb45dcced1ca"
+  instance_type          = "t3.medium"
   key_name               = "dvdstore_deployment"
   vpc_security_group_ids = [aws_security_group.dvdstore.id]
 
